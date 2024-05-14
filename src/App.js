dc98edc6965e65e6e5786410b0ce4324c12a9d20
leloufadel <leloufadel@gmail.com>
2024-05-14 12:22:57 +0000

    Fetch data from Question.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-function App() {
+const initialState = {
+  questions: [], 
+// 'error', 'ready. 'loading', 'success'
+  status: "loading",
+}
+function reducer(state, action){
+ switch(action.type){
+  case 'loading':
+    return { ...state, status: 'loading'};
+ }
+}
+export default function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+ useEffect(function(){
+ fetch("http://localhost:9000/questions")
+ .then((res) => res.json())
+ .then((data) => console.log(data))
+ .then((error) => console.error(error));
+
+ }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+    <Main >
+    <p>1/15</p>
+    <p>questions</p>
+    </Main>
     </div>
   );
 }
 
-export default App;
+
