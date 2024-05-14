69b8813b08453acb2c89e9783f91e11907ebbe4a
leloufadel <leloufadel@gmail.com>
2024-05-14 13:13:06 +0000

    StartScreen

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StartScreen() {
+  return (
+    <div className='start'>
+      <h2>Welcome to React Quiz App</h2>
+      <h3>questions to test your React mastery! </h3>
+   <button>Let's Start</button>
+    </div>
+  )
+}
