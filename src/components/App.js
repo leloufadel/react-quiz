4d043f0b7034828c8b394eedbd9e75d4120a20f7
leloufadel <leloufadel@gmail.com>
2024-05-15 08:57:24 +0000

    Display nextButton

--- src/components/App.js
+++ src/components/App.js
@@ -72,6 +72,7 @@ export default function App() {
             dispatch={dispatch}
             answer={answer}
           />
+          <NextButton answer={answer} dispatch={dispatch} />
 
           </>
          
